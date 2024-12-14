81cdf1c34033ca02592370f63f4a7b27f3b67a3e
DmytroKovinskyi <dmytro.kovinskyi@oa.edu.ua>
2024-12-14 20:12:24 +0000

    feat: done laboratory 14

--- app/Jobs/GenerateCatalog/GeneratePointsJob.php
+++ app/Jobs/GenerateCatalog/GeneratePointsJob.php
@@ -4,7 +4,7 @@ class GeneratePointsJob extends AbstractJob
 {
     public function handle()
     {
-        $f = 1 / 0; //симулюємо помилку
+        //$f = 1 / 0; //симулюємо помилку
         parent::handle();
     }
 }
