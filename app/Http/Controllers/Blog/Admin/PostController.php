16e7cb31791bf665f2a79d285e9e27ae91e7b619
DmytroKovinskyi <dmytro.kovinskyi@oa.edu.ua>
2024-12-14 19:25:09 +0000

    feat: done laboratory 12

--- app/Http/Controllers/Blog/Admin/PostController.php
+++ app/Http/Controllers/Blog/Admin/PostController.php
@@ -9,6 +9,8 @@ use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Models\BlogPost;
 use App\Http\Requests\BlogPostCreateRequest;
+use App\Jobs\BlogPostAfterCreateJob;
+use App\Jobs\BlogPostAfterDeleteJob;
 
 class PostController extends BaseController
 {
@@ -21,12 +23,17 @@ class PostController extends BaseController
      */
     private $blogCategoryRepository; // властивість через яку будемо звертатись в репозиторій категорій
 
+
     public function __construct()
     {
         parent::__construct();
-        $this->blogPostRepository = app(BlogPostRepository::class); //app вертає об'єкт класа
+        $this->blogPostRepository = app(BlogPostRepository::class);
         $this->blogCategoryRepository = app(BlogCategoryRepository::class);
     }
+
+    /**
+     * Display a listing of the resource.
+     */
     public function index()
     {
         $paginator = $this->blogPostRepository->getAllWithPaginate();
@@ -42,7 +49,6 @@ class PostController extends BaseController
         $item = new BlogPost();
         $categoryList = $this->blogCategoryRepository->getForComboBox();
 
-
         return view('blog.admin.posts.edit', compact('item', 'categoryList'));
     }
 
@@ -56,6 +62,7 @@ class PostController extends BaseController
         $item = (new BlogPost())->create($data); //створюємо об'єкт і додаємо в БД
 
         if ($item) {
+            BlogPostAfterCreateJob::dispatch($item);
             return redirect()
                 ->route('blog.admin.posts.edit', [$item->id])
                 ->with(['success' => 'Успішно збережено']);
@@ -102,7 +109,6 @@ class PostController extends BaseController
 
         $data = $request->all(); //отримаємо масив даних, які надійшли з форми
 
-
         $result = $item->update($data); //оновлюємо дані об'єкта і зберігаємо в БД
 
         if ($result) {
@@ -114,6 +120,7 @@ class PostController extends BaseController
                 ->with(['msg' => 'Помилка збереження'])
                 ->withInput();
         }
+
     }
 
     /**
@@ -126,6 +133,7 @@ class PostController extends BaseController
         //$result = BlogPost::find($id)->forceDelete(); //повне видалення з БД
 
         if ($result) {
+            BlogPostAfterDeleteJob::dispatch($id)->delay(now()->addSeconds(20));
             return redirect()
                 ->route('blog.admin.posts.index')
                 ->with(['success' => "Запис id[$id] видалено"]);
