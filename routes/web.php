7133c248bd5586a987d67ed10014266788d106a8
DmytroKovinskyi <dmytro.kovinskyi@oa.edu.ua>
2024-12-14 19:59:00 +0000

    feat: done laboratory 13

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,10 @@ Route::resource('rest', RestTestController::class)->names('restTest');
 Route::group([ 'namespace' => 'App\Http\Controllers\Blog', 'prefix' => 'blog'], function () {
     Route::resource('posts', PostController::class)->names('blog.posts');
     Route::group(['prefix' => 'digging_deeper'], function () {
+        Route::get('process-video', 'DiggingDeeperController@processVideo')
+            ->name('digging_deeper.processVideo');
+        Route::get('prepare-catalog', 'DiggingDeeperController@prepareCatalog')
+            ->name('digging_deeper.prepareCatalog');
         Route::get('collections', [DiggingDeeperController::class, 'collections'])
             ->name('digging_deeper.collections');
 
